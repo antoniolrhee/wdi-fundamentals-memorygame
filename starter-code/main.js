093f5203ee8a1ac418708f3cb796e4c98593a769
Antonio L. Rhee <antoniorhee@Antonios-MacBook-Pro.local>
2016-09-26 01:00:48 -0700

    unit10work

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,9 +4,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardFour) {
-	alert("You found a match!");
-} 
-else {
-	alert("Sorry, try again.");
+//if (cardOne === cardFour) {
+//	alert("You found a match!");
+//} 
+//else {
+//	alert("Sorry, try again.");
+//}
+
+var board = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i=0; i<4; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
 }
+createBoard();
